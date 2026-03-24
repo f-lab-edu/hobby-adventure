20f77d4cfa5d4f555a35dbffd73e3a40743f008f
zanny31 <jianb0331@naver.com>
2026-03-24 14:53:32 +0900

    chore: Spring Boot 프로젝트 초기 생성

---
+++ src/main/java/com/jian/hobbyadventure/HobbyAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.jian.hobbyadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyAdventureApplication.class, args);
+	}
+
+}
