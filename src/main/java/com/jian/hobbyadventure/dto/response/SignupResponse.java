be7d9dce02010b1f49cc3b7a6dd050b333b38e9c
JianBBB <sbganne@naver.com>
2026-03-27 19:51:26 +0900

    feat: 회원가입 요청/응답 DTO 구현

---
+++ src/main/java/com/jian/hobbyadventure/dto/response/SignupResponse.java
@@ -0,0 +1,11 @@
+package com.jian.hobbyadventure.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SignupResponse {
+
+    private final boolean success;
+}
