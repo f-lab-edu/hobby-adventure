98496fb0fa810b66bb89e28874ba1e8e1ae218eb
JianBBB <sbganne@naver.com>
2026-03-27 21:33:17 +0900

    feat: 회원가입 API 컨트롤러 구현

---
+++ src/main/java/com/jian/hobbyadventure/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.jian.hobbyadventure.controller;
+
+import com.jian.hobbyadventure.common.response.ApiResponse;
+import com.jian.hobbyadventure.dto.request.SignupRequest;
+import com.jian.hobbyadventure.dto.response.SignupResponse;
+import com.jian.hobbyadventure.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<SignupResponse>> signup(@RequestBody SignupRequest request) {
+        userService.signup(request);
+        return ResponseEntity.ok(ApiResponse.of(new SignupResponse(true)));
+    }
+}
