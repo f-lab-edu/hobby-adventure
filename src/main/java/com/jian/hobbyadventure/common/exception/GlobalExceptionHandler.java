c9c388ac5472fb1c50d2c9a38f7fe82cca92529a
JianBBB <sbganne@naver.com>
2026-04-05 07:08:02 +0900

    feat: 500 에러 핸들러에 로깅 추가

--- src/main/java/com/jian/hobbyadventure/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jian/hobbyadventure/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.jian.hobbyadventure.common.exception;
 
 import com.jian.hobbyadventure.common.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -23,6 +25,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unexpected error", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.of("서버 오류"));
     }
 }
