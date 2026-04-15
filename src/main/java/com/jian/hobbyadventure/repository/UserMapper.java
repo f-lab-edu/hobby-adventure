d4c62a69cb1052ef563fa0070c707edb4890b70f
JianBBB <sbganne@naver.com>
2026-04-15 19:39:43 +0900

    feat: 이메일로 사용자 조회 쿼리 추가

--- src/main/java/com/jian/hobbyadventure/repository/UserMapper.java
+++ src/main/java/com/jian/hobbyadventure/repository/UserMapper.java
@@ -1,10 +1,15 @@
 package com.jian.hobbyadventure.repository;
 
 import com.jian.hobbyadventure.domain.User;
+import org.apache.ibatis.annotations.Arg;
+import org.apache.ibatis.annotations.ConstructorArgs;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
@@ -14,6 +19,16 @@ public interface UserMapper {
 
     @Select("SELECT EXISTS(SELECT 1 FROM users WHERE nickname = #{nickname})")
     boolean existsByNickname(String nickname);
+
+    @ConstructorArgs({
+            @Arg(column = "user_id", javaType = Long.class),
+            @Arg(column = "email", javaType = String.class),
+            @Arg(column = "password", javaType = String.class),
+            @Arg(column = "nickname", javaType = String.class),
+            @Arg(column = "created_at", javaType = LocalDateTime.class)
+    })
+    @Select("SELECT * FROM users WHERE email = #{email}")
+    Optional<User> findByEmail(String email);
 
     @Insert("""
             INSERT INTO users (email, password, nickname)
