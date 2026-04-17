0ec4218205f1949bd92b74665848a0630c8b114e
JianBBB <sbganne@naver.com>
2026-04-17 19:27:46 +0900

    docs: 회원탈퇴 API Swagger 문서화

--- src/main/java/com/jian/hobbyadventure/controller/UserController.java
+++ src/main/java/com/jian/hobbyadventure/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.jian.hobbyadventure.controller;
 
 import com.jian.hobbyadventure.common.response.CommonResponse;
+import com.jian.hobbyadventure.common.response.ErrorResponse;
 import com.jian.hobbyadventure.dto.response.DeleteAccountResponse;
 import com.jian.hobbyadventure.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Tag(name = "User", description = "사용자 API")
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
@@ -19,6 +27,11 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원탈퇴")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원탈퇴 성공"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class)), description = "존재하지 않는 사용자")
+    })
     @DeleteMapping("/{userId}")
     public ResponseEntity<CommonResponse<DeleteAccountResponse>> deleteAccount(@PathVariable Long userId) {
         DeleteAccountResponse response = userService.deleteAccount(userId);
