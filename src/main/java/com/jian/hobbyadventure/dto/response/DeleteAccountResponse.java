7dc2adc14456978f56940dd0af1ee6e9026f9dfd
JianBBB <sbganne@naver.com>
2026-04-17 19:25:01 +0900

    feat: DeleteAccountResponse DTO 추가

---
+++ src/main/java/com/jian/hobbyadventure/dto/response/DeleteAccountResponse.java
@@ -0,0 +1,11 @@
+package com.jian.hobbyadventure.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeleteAccountResponse {
+
+    private final boolean success;
+}
