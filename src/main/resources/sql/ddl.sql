af9e6f544f48ae472541eced42a3daac0c22e613
JianBBB <sbganne@naver.com>
2026-04-28 19:20:08 +0900

    feat: categories 테이블 생성 및 초기 데이터 추가

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -9,3 +9,24 @@ CREATE TABLE users
     UNIQUE KEY uk_users_email (email),
     UNIQUE KEY uk_users_nickname (nickname)
 );
+
+CREATE TABLE categories
+(
+    category_id   BIGINT      NOT NULL AUTO_INCREMENT,
+    code          VARCHAR(50) NOT NULL,
+    name          VARCHAR(50) NOT NULL,
+    display_order INT         NOT NULL,
+    PRIMARY KEY (category_id),
+    UNIQUE KEY uk_categories_code (code),
+    UNIQUE KEY uk_categories_display_order (display_order)
+);
+
+INSERT INTO categories (code, name, display_order)
+VALUES ('EXERCISE', '운동', 1),
+       ('VISIT', '방문', 2),
+       ('GATHERING', '모임', 3),
+       ('CREATION', '창작', 4),
+       ('LEARNING', '학습', 5),
+       ('APPRECIATION', '감상', 6),
+       ('REST', '휴식', 7),
+       ('ETC', '기타', 8);
