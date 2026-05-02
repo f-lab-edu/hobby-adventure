d81965b6532ee06c3f4f6c7e29234f8efa36b04f
JianBBB <sbganne@naver.com>
2026-05-02 16:28:28 +0900

    refactor: DML을 ddl.sql에서 dml.sql로 분리

--- src/main/resources/sql/ddl.sql
+++ src/main/resources/sql/ddl.sql
@@ -23,12 +23,3 @@ CREATE TABLE categories
     UNIQUE KEY uk_categories_display_order (display_order)
 );
 
-INSERT INTO categories (code, name, display_order)
-VALUES ('EXERCISE', '운동', 1),
-       ('VISIT', '방문', 2),
-       ('GATHERING', '모임', 3),
-       ('CREATION', '창작', 4),
-       ('LEARNING', '학습', 5),
-       ('APPRECIATION', '감상', 6),
-       ('REST', '휴식', 7),
-       ('ETC', '기타', 8);
---
+++ src/main/resources/sql/dml.sql
@@ -0,0 +1,11 @@
+SET NAMES 'utf8mb4';
+
+INSERT INTO categories (code, name, display_order)
+VALUES ('EXERCISE', '운동', 1),
+       ('VISIT', '방문', 2),
+       ('GATHERING', '모임', 3),
+       ('CREATION', '창작', 4),
+       ('LEARNING', '학습', 5),
+       ('APPRECIATION', '감상', 6),
+       ('REST', '휴식', 7),
+       ('ETC', '기타', 8);
